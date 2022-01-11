89b7bf52ce905788fefc1e6072bbd7901079122e
Kimaniantony <kimaniantoni@gmail.com>
2022-01-11 23:21:23 +0300

    desktop changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const navMenu = document.querySelector('.nav-menu');
 const navToggle = document.querySelector('.navToggle');
 const navClose = document.querySelector('.navClose');
+const sideToggle = document.querySelector('.sideToggle');
+const sideClose = document.querySelector('.sideClose');
 
 navToggle.addEventListener('click',()=>{
     navMenu.style.transform = 'translateX(20px)';
@@ -9,9 +11,10 @@ navClose.addEventListener('click',()=>{
     navMenu.style.transform = 'translateX(500px)';
     navClose.style.transform = 'rotateX(180deg)';
 })
+sideToggle.addEventListener('click',()=>{
+    navMenu.style.transform = 'translateX(0px)';
+})
+sideClose.addEventListener('click',()=>{
+    navMenu.style.transform = 'translateX(-140px)';
+})
 
-const skillSet = [
-    {
-        
-    }
-]
